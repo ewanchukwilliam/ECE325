4792911be018fa84a33ec7fd1af18bb60cc6d1e2
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-09-19 14:51:51 -0600

    added new requirements for the code

--- Lab1/ece325_lab_assignment1/lab_assignment1/src/ece325/labs/lab1/Main.java
+++ Lab1/ece325_lab_assignment1/lab_assignment1/src/ece325/labs/lab1/Main.java
@@ -2,7 +2,6 @@ import ece325.labs.lab1.BandNameGenerator;
 
 public class Main {
   public static void main(String[] args) {
-    String[] files = new String[]{};
-    BandNameGenerator.main(files);
+    BandNameGenerator.main(new String[]{});
   }
 }
